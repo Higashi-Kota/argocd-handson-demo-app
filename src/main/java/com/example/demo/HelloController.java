449898dbbebb56f95d2a9a83e6187f0cb0f07fec
Higashi-Kota <higashi.kota@gri.jp>
2026-04-26 14:01:14 +0900

    Update HelloController to return [v2] suffix

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -15,6 +15,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, " + message + "!\n";
+        return "Hello, " + message + "! [v2]\n";
     }
 }
